9de956904b5797d2ff0d21761293ecb1f159dad9
Aphomer <aaphomer@gmail.com>
2025-05-14 07:27:49 +0100

    init constant....

--- programs/lending/src/constants.rs
+++ programs/lending/src/constants.rs
@@ -2,7 +2,7 @@ use anchor_lang::prelude::*;
 
 #[constants]
 pub const SOL_USB_FEED_ID: &str = "0x7d9e2258cec229cf52873a8e58d035a276873c485d753860e56d248fb33ce68a";
-pub const USDC_USD_FEED_ID: &str = "0xef0d8b6fda2ceba41da15d4095d1da392a0d2f8ed0c6c7bc0f4cfac8c280b56d"
+pub const USDC_USD_FEED_ID: &str = "0xef0d8b6fda2ceba41da15d4095d1da392a0d2f8ed0c6c7bc0f4cfac8c280b56d";
 pub const MAX_AGE: u64 = 100;
 
 
