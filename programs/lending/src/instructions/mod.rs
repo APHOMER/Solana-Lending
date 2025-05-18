4daf184a02a927ce28e5cc28eb1f93217c388c4d
Aphomer <aaphomer@gmail.com>
2025-05-18 07:48:39 +0100

    Initialize Mo from Instruction......

--- programs/lending/src/instructions/mod.rs
+++ programs/lending/src/instructions/mod.rs
@@ -1,7 +1,9 @@
-pub  use admin::*;
-pub mod  admin;
+pub use admin::*;
+pub mod admin;
 // pub mod state;
+pub use instruction;
 pub mod instruction;
+// mod instruction;
 pub use deposit::*;
 mod deposit;
 
@@ -9,4 +11,12 @@ pub use withdraw::*;
 mod withdraw;
 pub use borrow::*;
 mod borrow;
+pub use repay::*;
+pub mod repay;
+pub use liquidate::*;
+pub mod liquidate;
+
+
+
+
 
