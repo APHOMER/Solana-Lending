ed89b81e2289f1d6bc5d0d63a93925da0d4fb437
Aphomer <aaphomer@gmail.com>
2025-06-01 17:20:02 +0100

    implemented last updated borrow in User Struct

--- programs/lending/src/state.rs
+++ programs/lending/src/state.rs
@@ -14,7 +14,9 @@ pub struct User {
     pub borrowed_usdc_shares: u64,
     pub usdc_address: Pubkey,
     pub last_updated: i64,
+    pub last_updated_borrow: i64,
 }
+
 
 #[account]
 #[derive(InitSpace)]
